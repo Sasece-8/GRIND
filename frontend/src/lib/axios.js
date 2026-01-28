a8bf9be6b2739358d1e7650f976f505bf3a49475
Sasece_8 <ts932004@gmail.com>
2026-01-28 13:47:16 +0530

    update axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
 const api = axios.create({
-  baseURL: "http://localhost:3000" || process.env.VITE_BASE_URL,
+  baseURL: process.env.VITE_BASE_URL ||"http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
